d809ad61a8708d271f2c45fb08fac390eafd0739
Michael Short <michaelshort@pro3-1152.setonchs.org>
2018-12-05 08:03:03 -0700

    Fixed prefixes bug on transform in JS and CSS

--- public/index.js
+++ public/index.js
@@ -26,7 +26,6 @@ $('document').ready(function () {
 			case ("rock"):
 				switch (rightHand) {
 					case ("paper"):
-						document.getElementById("icon-right").color = "green";
 						return "Rock is covered by Paper!";
 					case ("scissors"):
 						return "Rock crushes Scissors!";
@@ -117,7 +116,11 @@ $('document').ready(function () {
 			 duration: 350,
 			 step: function (now) {
 				 $("#" + id).css({
-					 transform: "rotateZ(" + now + "deg)"
+					 transform: "rotateZ(" + now + "deg)",
+					 -webkit-transform: "rotateZ(" + now + "deg)",
+					 -ms-transform: "rotateZ(" + now + "deg)",
+					 -moz-transform: "rotateZ(" + now + "deg)",
+					 -o-transform: "rotateZ(" + now + "deg)"
 				 })
 			 },
 			 complete: completeFunction
